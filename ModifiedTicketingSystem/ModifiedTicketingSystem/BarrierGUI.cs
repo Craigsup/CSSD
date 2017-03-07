c4b1e70fb2a66dc96d8a99107cd89d808da10eab
Gaz1988 <b3028718@my.shu.ac.uk>
2017-03-07 22:25:58 +0000

    ...merging

---
+++ ModifiedTicketingSystem/ModifiedTicketingSystem/BarrierGUI.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ModifiedTicketingSystem.Properties;
+
+namespace ModifiedTicketingSystem {
+    public partial class BarrierGUI : Form {
+        private Scanner _scanner;
+        private Barrier _barrier;
+
+        public BarrierGUI() {
+            InitializeComponent();
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e) {
+            if (tbTicketId.Text != "") {
+                bool type = _scanner.GetScannerType();
+                if (type == true) {
+                    Station station = _scanner.GetStation();
+                    //_scanner.ValidateTicket();
+                } else {
+
+                }
+            }
+        }
+    }
+}
